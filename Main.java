3f344e2d5ef9ddc4fc61b80d388602dc414ced38
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 20:12:07 +0530

    add Main class to create instances of all duck types

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // creating objects of all ducks
+        MallardDuck md = new MallardDuck();
+        RedHeadDuck rhd = new RedHeadDuck();
+        RubberDuck rd = new RubberDuck();
+        DecoyDuck dd = new DecoyDuck();
+    }
+
+}
