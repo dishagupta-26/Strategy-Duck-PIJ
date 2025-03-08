1648df318b482a75f48df225adbea196f0ae1ba6
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 20:16:22 +0530

    add behavior methods for all duck instances in Main class

--- Main.java
+++ Main.java
@@ -10,12 +10,24 @@ public class Main {
         // calling display method of all ducks
         System.out.println("------------------");
         md.display();
+        md.performQuack();
+        md.performSwim();
+        md.performFly();
         System.out.println("------------------");
         rhd.display();
+        rhd.performQuack();
+        rhd.performSwim();
+        rhd.performFly();
         System.out.println("------------------");
         rd.display();
+        rd.performQuack();
+        rd.performSwim();
+        rd.performFly();
         System.out.println("------------------");
         dd.display();
+        dd.performQuack();
+        dd.performSwim();
+        dd.performFly();
         System.out.println("------------------");
     }
 }
