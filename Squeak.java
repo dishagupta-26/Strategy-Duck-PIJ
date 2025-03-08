930076d78cd4a018b8630fa3f04bc7f7016ab7be
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 19:20:52 +0530

    add Squeak class implementing QuackBehaviour interface

---
+++ Squeak.java
@@ -0,0 +1,7 @@
+// Squeak class that implements QuackBehaviour interface
+public class Squeak implements QuackBehaviour {
+    public void quack() {
+        System.out.println("Squeakkkkkkkkkkkkkkkk!");
+    }
+
+}
