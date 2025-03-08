ce352a5c771ba92bec597397b3717de4ce5b7127
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 20:15:03 +0530

    update display method in DecoyDuck class to reflect correct identity

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -10,6 +10,6 @@ public class DecoyDuck extends Duck {
 
     @Override
     public void display() {
-        System.out.println("I am a Rubber Duck!");
+        System.out.println("I am a Decoy Duck!");
     }
 }
