ff771a8d298e62eb86dcf04b3edff08c26fb6ec5
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 20:14:23 +0530

    add display method calls for all duck instances in Main class

--- Main.java
+++ Main.java
@@ -6,6 +6,16 @@ public class Main {
         RedHeadDuck rhd = new RedHeadDuck();
         RubberDuck rd = new RubberDuck();
         DecoyDuck dd = new DecoyDuck();
-    }
 
+        // calling display method of all ducks
+        System.out.println("------------------");
+        md.display();
+        System.out.println("------------------");
+        rhd.display();
+        System.out.println("------------------");
+        rd.display();
+        System.out.println("------------------");
+        dd.display();
+        System.out.println("------------------");
+    }
 }
