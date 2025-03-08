1006b0a75eab7933eef75e365880c52e88232342
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-08 19:27:56 +0530

    add FlyNoWay class implementing FlyBehaviour interface

---
+++ FlyNoWay.java
@@ -0,0 +1,6 @@
+// FlyNoWay class implementing FlyBehaviour interface
+public class FlyNoWay implements FlyBehaviour {
+    public void fly() {
+        System.out.println("I can't fly!");
+    }
+}
